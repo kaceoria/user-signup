38c2d02f8f60f18d1b4dfbc75f491873c153b55d
Kaycee Williams <kaceoria@icloud.com>
2019-07-24 23:04:11 -0500

    yo i think it works right

--- main.py
+++ main.py
@@ -14,26 +14,26 @@ def is_valid_email(address):
     if len(address) < 3 or len(address) > 20:
         return False
 
-        at = "@"
-        at_count = address.count(at)
-        if at_count != 1:
-            return False
+    at = "@"
+    at_count = address.count(at)
+    if at_count != 1:
+        return False
         
-        dot = "."
-        dot_count = address.count(dot)
-        if dot_count != 1:
-            return False
+    dot = "."
+    dot_count = address.count(dot)
+    if dot_count != 1:
+        return False
         
-        space = " "
-        space_count = address.count(space)
-        if space_count != 0:
-            return False
+    space = " "
+    space_count = address.count(space)
+    if space_count != 0:
+        return False
 
-        else:
-            return True
+    else:
+        return True
 
 
-@app.route("/signup", methods = ['POST'])
+@app.route("/signup", methods = ['POST', 'GET'])
 def signup():
     
     username = request.form['username']
@@ -57,7 +57,7 @@ def signup():
         password = ''
         verify = ''
     
-    if len(password) <3 or len(password) > 20:
+    if len(password) < 3 or len(password) > 20:
         password_error = 'password must be between 3 and 20 characters'
         password = ''
         verify = ''
@@ -75,6 +75,7 @@ def signup():
             email_error = "Please enter a vaild email address"
             password = ''
             verify = ''
+
     if not username_error and not password_error and not verify_error and not email_error:
         username = request.form['username']
         return redirect('/welcome?username={0}'.format(username))
@@ -89,5 +90,5 @@ def welcome():
     username = request.args.get('username')
     return render_template('welcome_user.html', username=username)
 
-
-app.run()
+if  __name__ == "__main__":
+    app.run()
